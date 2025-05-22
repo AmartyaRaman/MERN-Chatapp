9e99b9efc826989587087b66ca477ac85c76d2ca
Amartya <amartyatheking@gmail.com>
2025-05-22 23:45:37 +0530

    Deployment stage

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import dotenv from 'dotenv';
 import express from 'express';
 import cookieParser from 'cookie-parser';
@@ -13,6 +14,8 @@ import {app, server} from './socket/socket.js';
 dotenv.config();
 const PORT = process.env.PORT || 7000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());  // to parse json payload (from req.body)
 app.use(cookieParser());
 
@@ -20,11 +23,11 @@ app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
 app.use('/api/users', userRoutes)
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
-app.get('/', (req, res) => {
-  res.send("Hello");
-});
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
   console.log("Server started at port:", PORT);
