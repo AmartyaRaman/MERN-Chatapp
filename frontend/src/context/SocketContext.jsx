ceeab72ce78f79a5aa33efb29bc4ac8be403ed64
Amartya <amartyatheking@gmail.com>
2025-05-25 09:28:46 +0530

    socket bug fix

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({children}) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:8000", {
+      const socket = io("https://mern-chatapp-t6eo.onrender.com/", {
         query: {
           userId: authUser._id
         }
